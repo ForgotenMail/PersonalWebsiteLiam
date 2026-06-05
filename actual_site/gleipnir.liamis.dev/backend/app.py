8c0f013aa8170789be95bf6407d7b1fa6dde69b4
Forgotten_Mail <william.thomas.ribble@gmail.com>
2026-06-05 13:40:26 -0700

    Added pyscopg and then made the basic main functions of the program

--- actual_site/gleipnir.liamis.dev/backend/app.py
+++ actual_site/gleipnir.liamis.dev/backend/app.py
@@ -1,8 +1,22 @@
 from flask import Flask,  jsonify, request
 from flask_cors import CORS
+import psycopg
 
 
 
+with psycopg.connect("dbname=liam user=liam") as conn:
+
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+
+        # Execute a command: this creates a new table
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS task (
+                task_name text PRIMARY KEY,
+                completed BOOLEAN DEFAULT FALSE
+                )
+            """)
+        
 app = Flask(__name__)
 CORS(app)
 
@@ -18,6 +32,53 @@ def receive_message():
 
     user_message = data['message']
     print(f"Message from frontend: {user_message}")
+
+    return jsonify({
+        "status": "sucsess",
+        "reply": f"Flask recieved your message dude: {user_message}"
+
+    })
+
+
+@app.route('/addelement', methods=['POST'])
+
+def add_element():
+    data = request.get_json()
+
+    if not data or 'message' not in data:
+        return jsonify({
+            "status": "You left me hanging bro with no message :("
+        })
+
+    user_message = data['message']
+    cur.execute(
+        "INSERT INTO task (task_name) VALUES (%s)",
+        (user_message))
+    return jsonify({
+        "status": "sucsess",
+        "reply": f"Flask recieved your message dude: {user_message}"
+
+    })
+
+@app.route('/deletetask', methods=['POST'])
+
+def delete_element():
+    data = request.get_json()
+
+    if not data or 'message' not in data:
+        return jsonify({
+            "status": "You left me hanging bro with no message :("
+        })
+    try:
+
+        user_message = data['message']
+        cur.execute("""
+                    DELETE FROM task WHERE task_name = %s;
+                    """
+                    [(user_message)]
+                    )
+    except:      
+        return jsonify({"error": "Task that was sent does not exist"})
 
     return jsonify({
         "status": "sucsess",
